2587f69e16830d67a268694a385c696098f1d7bf
AliakseiSerhiyenia <alexeisergienia@gmail.com>
2024-01-28 22:19:20 +0300

    feat: add read method. use readFile

--- node-nodejs-basics-main/src/fs/read.js
+++ node-nodejs-basics-main/src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const data = await fs.readFile(filePath, {encoding: 'utf-8'});
+        console.log(data)
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
