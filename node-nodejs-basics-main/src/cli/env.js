a48014ef2e435ed71b45de213e55a9bb1d422d83
AliakseiSerhiyenia <alexeisergienia@gmail.com>
2024-01-28 22:29:54 +0300

    feat: add parseEnv method

--- node-nodejs-basics-main/src/cli/env.js
+++ node-nodejs-basics-main/src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const args = process.env;
+    const resultArr = [];
+
+    for (let key in args) {
+        if (key.startsWith('RSS_')) {
+            resultArr.push(`${key}=${args[key]}`);
+        }
+    }
+
+    console.log(resultArr.join('; '));
 };
 
 parseEnv();
