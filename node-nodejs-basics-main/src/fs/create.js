570f37d6b4964dd627bdb18d7dab117670fe78ff
AliakseiSerhiyenia <alexeisergienia@gmail.com>
2024-01-28 21:27:43 +0300

    feat: add create method

--- node-nodejs-basics-main/src/fs/create.js
+++ node-nodejs-basics-main/src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.appendFile(filePath, 'I am fresh and young', {flag: 'wx'}, (error, data) => {
+            console.log('file created')
+        });
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
