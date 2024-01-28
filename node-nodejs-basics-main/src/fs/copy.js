04146ff2dfcbca28b3b14c5e5ac25c928408872a
AliakseiSerhiyenia <alexeisergienia@gmail.com>
2024-01-28 21:50:26 +0300

    feat: add copy method. use experimental cp

--- node-nodejs-basics-main/src/fs/copy.js
+++ node-nodejs-basics-main/src/fs/copy.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sourceFilePath = path.join(__dirname, 'files');
+const resultDirPath = path.join(__dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.cp(sourceFilePath, resultDirPath, {recursive: true, errorOnExist: true, force: false}, (error, data) => {
+            console.log('file created');
+        });
+    } catch (err) {
+        throw Error('FS operation failed');
+    }
 };
 
 await copy();
