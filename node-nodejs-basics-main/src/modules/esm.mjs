f2ee1078a607d0ebf8c503b34d87fdbe7826a16a
AliakseiSerhiyenia <alexeisergienia@gmail.com>
2024-01-29 11:20:36 +0300

    feat: add modules task

--- node-nodejs-basics-main/src/modules/esm.mjs
+++ node-nodejs-basics-main/src/modules/esm.mjs
@@ -1,16 +1,22 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c.js');
+import path from 'path';
+import {release, version} from 'os';
+import {createServer as createServerHttp} from 'http';
+import './files/c.js';
+import {fileURLToPath} from 'url';
+import fileA from './files/a.json' assert {type: 'json'};
+import fileB from './files/b.json' assert {type: 'json'};
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = fileA;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = fileB;
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +39,8 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
-    myServer,
+    myServer
 };
 
