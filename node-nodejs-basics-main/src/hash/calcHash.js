bab0b52e52fa3cd41a0c435e253972ea7bfb2ba1
AliakseiSerhiyenia <alexeisergienia@gmail.com>
2024-01-29 22:40:04 +0300

    feat: add hash task

--- node-nodejs-basics-main/src/hash/calcHash.js
+++ node-nodejs-basics-main/src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import * as fs from 'node:fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import {createHash} from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const nodeReadStream = fs.createReadStream(filePath, {encoding: 'utf-8'});
+        const hash = createHash('sha256').setEncoding('hex');
+
+        hash.on('finish', () => console.log(hash.read()));
+
+        nodeReadStream.pipe(hash);
+    } catch (err) {
+        throw Error('FS operation failed');
+    }
 };
 
 await calculateHash();
