38307a364ca567ab646cf1194b3ed864b5e5052d
AliakseiSerhiyenia <alexeisergienia@gmail.com>
2024-01-30 19:15:58 +0300

    feat: add Child Processes task

--- node-nodejs-basics-main/src/cp/cp.js
+++ node-nodejs-basics-main/src/cp/cp.js
@@ -1,6 +1,23 @@
+import {spawn} from 'child_process';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const scriptFilePath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [scriptFilePath, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    child.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
